a093e1834c715d167c92b022e00969a0bba042a6
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-05-25 16:41:26 +0200

    add basic state machine

---
+++ Assets/Core/Scripts/Controllers/StateMachines/State.cs
@@ -0,0 +1,9 @@
+namespace Core.Scripts.Controllers.StateMachines
+{
+    public abstract class State
+    {
+        public abstract void Enter();
+        public abstract void Tick(float deltaTime);
+        public abstract void Exit();
+    }
+}
