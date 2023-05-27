0900070d6648a816717cf571acde5b9d14eb97bf
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-05-27 10:09:44 +0200

    add inputReader

--- Assets/Core/Scripts/Controllers/InputReader.cs
+++ Assets/Core/Scripts/Controllers/InputReader.cs
@@ -1,9 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Core.Scripts.Controllers
 {
-    public class InputReader : MonoBehaviour
+    public class InputReader : MonoBehaviour, Controls.IPlayerActions
     {
-        
+        #region Statements
+
+        public Vector2 MoveValue { get; private set; }
+        public Vector2 LookValue { get; private set; }
+
+        #endregion
+
+        #region Events
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            MoveValue = context.ReadValue<Vector2>();
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            LookValue = context.ReadValue<Vector2>();
+        }
+
+        #endregion
     }
 }
