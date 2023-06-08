ffe4445739f3d4acdcb0865e2bd3e65a5cda5622
LaM0uette <LaM0uette.dev@gmail.com>
2023-06-08 14:59:04 +0200

    up

--- Assets/Core/Scripts/Controllers/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Core/Scripts/Controllers/StateMachines/Player/PlayerStateMachine.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using Cinemachine;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Core.Scripts.Controllers.StateMachines.Player
 {
@@ -46,7 +45,6 @@ namespace Core.Scripts.Controllers.StateMachines.Player
         [Range(0f, -180f)] public float BottomClamp = -30.0f;
         public GameObject _cinemachineCameraTarget;
         public CinemachineVirtualCamera[] CinemachineCameras;
-        public CinemachineStateDrivenCamera CinemachineStateDrivenCamera;
         public float MinZoom = 1f;
         public float MaxZoom = 8f;
         public float ZoomForce = 10f;
@@ -75,9 +73,7 @@ namespace Core.Scripts.Controllers.StateMachines.Player
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
-            
-            Inputs.SwitchCameraEvent += () => { CinemachineStateDrivenCamera.enabled = !CinemachineStateDrivenCamera.enabled; };
-            
+
             SwitchState(new PlayerMoveState(this));
         }
 
