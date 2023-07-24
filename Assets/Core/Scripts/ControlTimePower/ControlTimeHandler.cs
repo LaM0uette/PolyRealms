07816b977d596f871a3711720207cda4536f2d01
LaM0uette <LaM0uette.dev@gmail.com>
2023-07-24 08:48:41 +0200

    test modify ages building

--- Assets/Core/Scripts/ControlTimePower/ControlTimeHandler.cs
+++ Assets/Core/Scripts/ControlTimePower/ControlTimeHandler.cs
@@ -5,6 +5,19 @@ namespace Core.Scripts.ControlTimePower
     public class ControlTimeHandler : MonoBehaviour
     {
         #region Statements
+        
+        private int _currentAge;
+
+        private GameObject _childMiddleAge;
+        private GameObject _childModernAge;
+        private GameObject _childFuturAge;
+
+        private void Awake()
+        {
+            _childMiddleAge = gameObject.transform.GetChild(0).gameObject;
+            _childModernAge = gameObject.transform.GetChild(1).gameObject;
+            _childFuturAge = gameObject.transform.GetChild(2).gameObject;
+        }
 
         private void OnEnable()
         {
@@ -18,9 +31,36 @@ namespace Core.Scripts.ControlTimePower
 
         #endregion
 
+        #region Functions
+
         private void OnControlTimeSwitchEvent()
         {
-            Debug.Log("ControlTimeHandler: OnControlTimeSwitchEvent");
+            ResetAges();
+            
+            switch (_currentAge)
+            {
+                case 0:
+                    _childModernAge.SetActive(true);
+                    _currentAge++;
+                    break;
+                case 1:
+                    _childFuturAge.SetActive(true);
+                    _currentAge++;
+                    break;
+                case 2:
+                    _childMiddleAge.SetActive(true);
+                    _currentAge = 0;
+                    break;
+            }
         }
+
+        private void ResetAges()
+        {
+            _childMiddleAge.SetActive(false);
+            _childModernAge.SetActive(false);
+            _childFuturAge.SetActive(false);
+        }
+
+        #endregion
     }
 }
